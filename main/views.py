17ed5576db3f13ca0d09eeb9bcb28ed2db39a10e
ze666ro <jesus666saves@hotmail.com>
2023-12-25 15:22:32 +0300

    created base.html and main app

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
+        'title': 'Home - Главная',
         'content':'Главная страница магазина - Home',
     }
     return render(request, 'main/index.html', context)
