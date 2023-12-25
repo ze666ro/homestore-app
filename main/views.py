36590c4379fbb307dc9c2d5762b966b2ebe89602
ze666ro <jesus666saves@hotmail.com>
2023-12-25 13:34:34 +0300

    example commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content':'Главная страница магазина - Home',
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse("About Page")
