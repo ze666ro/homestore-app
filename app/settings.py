7d59ca5e98b36bf5d20b20477ffb8d37cd0a0dac
ze666ro <jesus666saves@hotmail.com>
2023-12-28 22:01:56 +0300

    bug fixes

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +131,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
