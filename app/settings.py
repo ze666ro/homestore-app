720854d58961b760a87d4b6a2a5877af9271767e
ze666ro <jesus666saves@hotmail.com>
2024-01-08 00:24:05 +0300

    migrate from sqllite to postrgresql

--- app/settings.py
+++ app/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
